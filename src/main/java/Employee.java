5dea37b055a7361865f1cc35d224c3cb522fbecf
usha-12 <usha12ahirwar@gmail.com>
2023-02-26 23:00:08 +0530

    Added: practice json file and pojo class

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    private String name;
+    private String id;
+    private double sal;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public double getSal() {
+        return sal;
+    }
+    public void setSal(double sal) {
+        this.sal = sal;
+    }
 }
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class MainClass {
+public class MainClass {
+    public static void main(String[] args) {
+        // Create an Employee class object
+        Employee obj= new Employee();
+        obj.setName("Alisha"); // Setting the values using the set() method
+        obj.setId("A001");
+        obj.setSal(200000);
+        System.out.println("Name: "+ obj.getName()); //Getting the values using the get() method
+        System.out.println("Id: " + obj.getId());
+        System.out.println("Salary: " +obj.getSal());
+    }
 }
